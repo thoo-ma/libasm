0f1cd8ecd170b52794c82a9edbd3380463b0b566
thoo-ma <trobin@student.42.fr>
2025-01-09 14:04:19 +0100

    cleaner strlen

--- ft_strlen.asm
+++ ft_strlen.asm
@@ -2,22 +2,17 @@ section .text
   global ft_strlen
 
 ft_strlen:
+  push  rcx             ; save rcx
+  xor   rcx, rcx        ; init rcx
 
-  push  rcx            ; save and clear out counter
-  xor   rcx, rcx
+.loop:
+  cmp   byte [rdi], 0
+  jz    .exit
+  inc   rcx
+  inc   rdi
+  jmp   .loop
 
-ft_strlen_next:
-
-  cmp   [rdi], byte 0  ; null byte yet?
-  jz    ft_strlen_null ; yes, get out
-
-  inc   rcx            ; char is ok, count it
-  inc   rdi            ; move to next char
-  jmp   ft_strlen_next ; process again
-
-ft_strlen_null:
-
-  mov   rax, rcx       ; rcx = the length (put in rax)
-
-  pop   rcx            ; restore rcx
-  ret                  ; get out
+.exit:
+  mov   rax, rcx
+  pop   rcx             ; restore rcx
+  ret
